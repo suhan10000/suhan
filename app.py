e5bfca793db0a2f046df46c41db85949b7ff4ac4
suhan10000 <bogeji121@gmail.com>
2026-07-11 15:29:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ user_id = st.text_input("이름")
 grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
 ban = st.number_input("반", min_value=1, max_value=10, value=1)
 
-st.select_slider("난이도", options=["쉬움", "보통", "어려움"],value="보통")
+난의도=st.select_slider("난이도", options=["쉬움", "보통", "어려움"],value="보통")
 st.slider("점수")
 
 question = st.text_area("소감", placeholder="여기에 소감을 작성해 주세요.")
