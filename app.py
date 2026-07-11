94630d05d4b6e6acb6e81f1b260efb672040fcef
suhan10000 <bogeji121@gmail.com>
2026-07-11 15:26:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.markdown("# 앱 UI 만들기")
 st.markdown("---")
 user_id = st.text_input("이름")
-ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
-age = st.number_input("반", min_value=1, max_value=10, value=1)
+grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
+class = st.number_input("반", min_value=1, max_value=10, value=1)
 
 st.select_slider("난이도", options=["쉬움", "보통", "어려움"],value="보통")
 st.slider("점수")
@@ -17,13 +17,4 @@ if st.button("확인"):
     if agree:
         st.success(f"성공적으로 전송되었습니다! ({user_id}님)")
         st.markdown(f"""
-        * **질문 내용:** {question}
-        * **선택 모델:** `{ai_model}` | **말투:** `{tone}`
-        * **활성화 기능:** {', '.join(features) if features else '없음'}
-        * **창의성:** `{creativity}%` | **처리 속도:** `{ai_speed}`
-        """)
-        
-        if age < 14:
-            st.info("참고: 14세 미만 사용자이므로 보호자 모드가 활성화됩니다.")
-    else:
-        st.error("⚠️ 동의 항목에 체크해야 전송이 가능합니다.")
+       {user_id}/{grade}/{class}/{난의도}
