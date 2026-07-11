596ef70b688cb613da4a1280b341161fd325d593
suhan10000 <bogeji121@gmail.com>
2026-07-11 15:27:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.markdown("# 앱 UI 만들기")
 st.markdown("---")
 user_id = st.text_input("이름")
 grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
-class = st.number_input("반", min_value=1, max_value=10, value=1)
+ban = st.number_input("반", min_value=1, max_value=10, value=1)
 
 st.select_slider("난이도", options=["쉬움", "보통", "어려움"],value="보통")
 st.slider("점수")
@@ -16,4 +16,4 @@ st.markdown("---")
 if st.button("확인"):
         st.success(f"성공적으로 전송되었습니다! ({user_id}님)")
         st.markdown(f"""
-       {user_id}/{grade}/{class}/{난의도}
+       {user_id}/{grade}/{ban}/{난의도}
