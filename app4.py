343f5fedb7b1cf55d7a893ea9a03a84eaf30a149
suhan10000 <bogeji121@gmail.com>
2026-07-11 16:16:56 +0900

    Update app4.py

--- app4.py
+++ app4.py
@@ -5,4 +5,4 @@ if 'count' not in st.session_state:
     st.session_state.count=0
 if st.button("증가"):
     st.session_state.count +=1
-st.markdown(f"## 현재숫자: '{session_state.count}'")
+st.markdown(f"## 현재숫자: '{st.session_state.count}'")
