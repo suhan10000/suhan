ca1eed800c8c96a068fd5c322d02ce51244a2d63
suhan10000 <bogeji121@gmail.com>
2026-07-11 16:33:34 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def reset_all():
     st.session_state.user_name = ""
-    st.ssession_state.weather = "맑음"
+    st.session_state.weather = "맑음"
     st.session_state.top_type = "후드티"
     st.session_state.top_color = "밝음"
     st.session_state.bottom_type = "청바지"
